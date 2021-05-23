f120a76eeb770dbf0d23fefd016a5d8fc7f50635
AdrielIg <adrielignaciogomez@gmail.com>
2021-05-23 02:24:54 -0300

    Added vanilla-tilt to Intro

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,12 +1,15 @@
+import { useEffect, useRef } from 'react';
 //Components
 import SocialMediaButtons from './SocialMediaButtons';
 import AvatarLogo from './AvatarLogo'
 //Material UI
 
-import { Container } from '@material-ui/core'
+import { Container, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import VanillaTilt from 'vanilla-tilt';
+
 
 
 
@@ -16,33 +19,70 @@ const useStyles = makeStyles(() => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+
+
+
   },
   introTitle: {
     margin: '3rem 0 2rem 0',
     fontWeight: 500,
-    letterSpacing: '.5rem'
+    letterSpacing: '.5rem',
+
   },
   introProfile: {
     fontWeight: 200,
     fontSize: "2rem",
     opacity: .6,
-  }
-}))
 
+  },
+  containerIntroBox: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '1rem',
+
+  }
+
+}))
 
 
 
 
 const Intro = () => {
   const classes = useStyles()
+  const tiltRef = useRef()
+  const tiltChildrenRef = useRef()
+  useEffect(() => {
+    const tiltNode = tiltRef.current
+
+    console.log(tiltNode)
+    const vanillaTiltOption = {
+      max: 20,
+      speed: 10,
+      glare: true,
+      gyroscope: true,
+      'max-glare': 0.2,
+      transition: true,
+      reverse: true
+
+    }
+
+    VanillaTilt.init(tiltNode, vanillaTiltOption)
+
+
+  })
+
   return (
     <Container component='section' align='center' className={classes.introContainer} >
-      <AvatarLogo />
-      <Typography className={classes.introTitle} variant='h2' color='secondary' >Adriel Gomez</Typography>
-      <Typography className={classes.introProfile} variant='h3' color='secondary' >| Front End Developer |</Typography>
-      <SocialMediaButtons variant='contained' />
-      <ArrowDownwardIcon className='bouncer' color='secondary' fontSize='large' />
+      <Box ref={tiltRef} component='div' className={classes.containerIntroBox}>
+        <AvatarLogo />
+        <Typography className={classes.introTitle} variant='h2' color='secondary' >Adriel Gomez</Typography>
+        <Typography className={classes.introProfile} variant='h3' color='secondary' >| Front End Developer |</Typography>
+        <SocialMediaButtons variant='contained' />
+        <ArrowDownwardIcon className='bouncer' color='secondary' fontSize='large' />
+      </Box>
     </Container >
   )
 }
